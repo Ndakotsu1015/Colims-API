ddaf8da746fdd237b0cac98585f4eb48bcd7cb5e
Phalcorine <phalcorine@gmail.com>
2022-01-25 10:23:21 +0100

    Adjusted migrations for award letter models -> approved by field...

--- database/migrations/2022_01_25_092135_alter_table_adjust_approved_by_field.php
+++ database/migrations/2022_01_25_092135_alter_table_adjust_approved_by_field.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameAwardletterFields extends Migration
+class AlterTableAdjustApprovedByField extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,8 @@ class RenameAwardletterFields extends Migration
     public function up()
     {
         Schema::table('award_letters', function (Blueprint $table) {
-            $table->renameColumn('property_type_id', 'contract_type_id');
-            $table->foreign('contract_type_id')->references('id')->on('contract_types')->onDelete('cascade');
-            $table->renameColumn('posted_by', 'approved_by');
-            // $table->unsignedBigInteger('approved_by')->index()->change();
-            // $table->foreign('approved_by')->references('id')->on('employees')->onDelete('cascade');        
+            $table->unsignedBigInteger('approved_by')->index()->change();
+            $table->foreign('approved_by')->references('id')->on('employees')->onDelete('cascade');
         });
     }
 
