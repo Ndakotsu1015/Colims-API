2d8948806e161451c866a0a606b5f2e5cc21409c
idreesbalaumar <idreesbalaumar@gmail.com>
2022-03-26 12:55:45 +0100

    awardLetterInternalDocuments

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable 
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function awardLetterInternalDocuments()
+    {
+        return $this->hasMany(AwardLetterInternalDocument::class);
+    }
 }
