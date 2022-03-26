d922f70eac96e099b69ca9a04923f618afb2485a
kingabdul02 <kingabdul02@yahoo.com>
2022-03-26 13:38:51 +0100

    fix: awardletter posted by

--- database/migrations/2022_03_21_112252_create_award_letter_internal_documents_table.php
+++ database/migrations/2022_03_21_112252_create_award_letter_internal_documents_table.php
@@ -20,7 +20,7 @@ class CreateAwardLetterInternalDocumentsTable extends Migration
             $table->string('name');
             $table->string('filename');
             $table->foreignId('award_letter_id')->constrained();
-            $table->foreignId('posted_by')->constrained();
+            $table->foreignId('posted_by')->constrained('users');
             $table->softDeletes();
             $table->timestamps();
         });
